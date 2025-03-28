38cc8126b604dbead8e1df26d746b037f2c4284f
ParkMinSung2580 <114042812+ParkMinSung2580@users.noreply.github.com>
2025-03-28 18:02:00 +0900

    Data_Structure
    Data_Structure Pratice
    자료구조 개념 및 아래 자료구조 실습
    (List,LinkedList,Queue,Stack)

---
+++ CSharp-Study/CSharp-Study/Parentheses_Search.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Study
+{
+    internal class Parentheses_Search
+    {
+        static void Main(string[] args)
+        {
+            char[] open = { '(', '{', '[' };
+            char[] close = { ')', '}', ']' };
+            string input = Console.ReadLine(); //input
+            Stack<char> st = new Stack<char>();
+            string answer = "YES";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (open.Contains(input[i]))
+                {
+                    st.Push(input[i]);
+                }
+                else if (close.Contains(input[i]))
+                {
+                    if (!st.Any()) // 닫는 괄호가 있는데 스택이 비어있으면 오류
+                    {
+                        answer = "NO";
+                        break;
+                    }
+                    char lastOpen = st.Pop();
+                    if (Array.IndexOf(open, lastOpen) != Array.IndexOf(close, input[i])) //open배열에서 가장 마지막에 들어온 열린괄호와 닫힌괄호에서의 현재 input[i] 즉,닫힌괄호를 만나자마자 해당 괄호와 인덱스가 맞는지?  
+                    {
+                        answer = "NO";
+                        break;
+                    }
+                }
+            }
+
+            // 모든 괄호 처리가 끝났을 때 스택이 비어 있어야 정상
+            if (st.Any())
+            {
+                answer = "NO";
+            }
+
+            Console.WriteLine(answer);
+        }
+    }
+}
---
+++ CSharp-Study/CSharp-Study/Yosepus.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Study
+{
+    internal class Yosepus
+    {
+        /*
+         * 요세푸스 문제
+        n명이 동그랗게 모여있을때 숫자 k를 외치면 1번부터 시작해서 k번째 사람이 모임에서 빠지는 경우를 반복했을 때 
+        마지막에 남아 있는 사람을 구하는 문제       
+        
+        배열
+        리스트 
+        연결리스트 //연결리스트 뺀거 + 3 가면 다음순서
+         */
+
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+
+            Console.WriteLine("사람 수를 입력하세요");     //4줄
+            string input = Console.ReadLine();
+            int humanNum = int.Parse(input);
+            //Console.WriteLine(num);       //디버깅
+
+            //죽을 사람 
+            Console.WriteLine("빠지는 수 입력하세요");     //4줄
+            string input2 = Console.ReadLine();
+            int findNum = int.Parse(input2);
+            //Console.WriteLine(num2); 
+
+            for (int i = 0; i < humanNum; i++)
+            {
+                list.Add(i + 1);                //사람추가
+            }
+
+            //list 범위기반반복 리스트 잘들어갔는지 확인용
+            /*foreach(int num in list)
+            {
+                Console.WriteLine("{0}", num);
+            }*/
+
+            int index = 0; // 시작 인덱스
+            while (list.Count > 1)
+            {
+                // M-1 번째 사람을 찾음
+                index = (index + findNum - 1) % list.Count;
+                Console.WriteLine("제거된 사람: " + list[index]);
+                list.RemoveAt(index); // 해당 사람을 리스트에서 제거
+                Console.Write("남은 사람들 : ");
+                foreach (int num in list)
+                {
+                    Console.Write("{0} ", num);
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}

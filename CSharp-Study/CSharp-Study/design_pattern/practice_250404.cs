0c2f6e70ee80c549111bd7a2be03c31cdb849186
ParkMinSung2580 <114042812+ParkMinSung2580@users.noreply.github.com>
2025-04-04 18:04:10 +0900

    Factory,Builder(DesignPattern)
    빌더, 팩토리 ,체인메서드 실습

--- CSharp-Study/CSharp-Study/design_pattern/practice_250404.cs
+++ CSharp-Study/CSharp-Study/design_pattern/practice_250404.cs
@@ -8,7 +8,14 @@ using System.Xml.Linq;
 
 namespace CSharp_Study.design_pattern
 {
-    //프로토 타입
+    /*팩토리 패턴 실습
+     * WeaponFactory 제작을 시도해보자
+    1. 무기는 이름, 공격력, 공격 범위
+    2. 팩토리를 통해서 무기 생성을 진행하는 경우
+    (1) 철검, 나무창, 쇠도끼
+    +) 무기 팩토리에 희귀도를 추가하여 제작 할 수 있도록 하자
+    (1) 일반등급 +0% 데미지 추가, 희귀등급 +10%, 에픽 15퍼 전설등급: 20퍼
+
     internal class practice_250404
     {
         static void Main()
@@ -46,8 +53,8 @@ namespace CSharp_Study.design_pattern
                     case "쇠도끼": weapon = new Weapon("쇠도끼",500,1); break;
                     default:
                         //throw new ArgumentException("존재하지 않는 무기 이름입니다: {0}",name);
-                        /*throw new ArgumentException(string, string) 생성자는 포맷 문자열이 아니라,
-                        첫 번째는 메시지, 두 번째는 "내부 파라미터" 용으로 쓰기 때문입니다.*/
+                        //throw new ArgumentException(string, string) 생성자는 포맷 문자열이 아니라,
+                        //첫 번째는 메시지, 두 번째는 "내부 파라미터" 용으로 쓰기 때문입니다.
                         throw new ArgumentException($"존재하지 않는 무기 이름입니다: {name}");                        
                 }
                 weapon.SetRarity(rarity);
@@ -88,5 +95,98 @@ namespace CSharp_Study.design_pattern
                 Console.WriteLine("이름 : {0}, 무기 공격력 : {1}, 무기 공격범위 : {2}", name, attackPoint, attackRange);
             }
         }
-    }
+    }*/
+
+    /*
+    - 빌더 패턴 - 
+    Animal Builder
+    1. 동물은 이름, 생산품, 울음소리, 사료종류를 조합,
+    2. 여러 종류의 동물을 구현할 수 있도록 빌더를 제작해보자
+    동물의 종류 양,소,닭
+    +) 희귀종 동물도 만들수 있도록 하자.
+    (1) 희귀종 동물은 특수생산품을 생산하도록 구현하자.
+
+    // TODO : 희귀종 동물은 특수 생산품을 생산하도록 구현하기
+    */
+
+ internal class practice_250404
+ {
+     public static void Main()
+     {
+         AnimalBuilder sheepBuilder = new AnimalBuilder()
+             .SetName("양")
+             .SetProuct("양털")
+             .SetFeed("양 전용 사료");
+         AnimalBuilder chickenBuilder = new AnimalBuilder()
+             .SetName("닭")
+             .SetProuct("달걀")
+             .SetFeed("닭 전용 사료");
+         AnimalBuilder cowBuilder = new AnimalBuilder()
+            .SetName("소")
+            .SetProuct("소고기")
+            .SetFeed("소 전용 사료");
+
+         Animal sheep = sheepBuilder.Build();
+         Animal chicken = chickenBuilder.Build();
+         Animal cow = cowBuilder.Build();
+
+         sheep.Print();
+         chicken.Print();
+         cow.Print();
+     }
+
+     public class AnimalBuilder
+     {
+         public string name;
+         public string product;
+         public string feed;
+
+         public AnimalBuilder()
+         {
+             name = "default name";
+             product = "default product";
+             feed = "default feed";
+         }
+
+         public AnimalBuilder SetName(string name)
+         {
+             this.name = name;
+             return this;
+         }
+
+         public AnimalBuilder SetProuct(string product)
+         {
+             this.product = product;
+             return this;
+         }
+
+         public AnimalBuilder SetFeed(string feed)
+         {
+             this.feed = feed;
+             return this;
+         }
+
+         public Animal Build()
+         {
+             Animal animal = new Animal();
+             animal.Name = name;
+             animal.Product = product;
+             animal.Feed = feed;
+
+             return animal;
+         }
+     }
+
+     public class Animal
+     {
+         public string Name { get; set; }
+         public string Product { get; set; }
+         public string Feed { get; set; }
+
+         public void Print()
+         {
+             Console.WriteLine("이름 : {0}, 생산품 : {1}, 사료 : {2}",Name,Product,Feed);
+         }
+     }
+ }
 }

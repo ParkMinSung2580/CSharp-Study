092a57a06d5cba0885d8551e403e8b7ecee12c7e
ParkMinSung2580 <114042812+ParkMinSung2580@users.noreply.github.com>
2025-04-04 13:10:49 +0900

    Sort Alrgorism,Degisn Pattern
    Sort Alrgorism,Degisn Pattern

---
+++ CSharp-Study/CSharp-Study/Alrgorism.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Study
+{
+    internal class Alrgorism
+    {
+
+        //선택 삽입 버블 머지 퀵 힙 정렬 
+
+        //선택정렬
+        public static void SelectSort(ref int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int minIndex = i;
+                for (int j = i+1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[minIndex])
+                        minIndex = j;
+                }
+                 if (minIndex != i)
+                 {
+                    int temp = arr[i];
+                    arr[i] = arr[minIndex];
+                    arr[minIndex] = temp;
+                 }
+            }
+        }
+
+        //삽입정렬
+
+        public static void InsertSort(ref int[] arr)
+        {
+
+        }
+
+        public static void ArrPrint(int[] arr)
+        {
+            Console.WriteLine(string.Join(", ", arr));
+        }
+
+        public static void Main()
+        {
+            Random rand = new Random();
+            int[] arr = new int[20]; // 크기 10의 배열 생성
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(1, 100); // 1부터 99까지의 랜덤 값 저장
+            }
+            // 배열 출력
+            SelectSort(ref arr);
+            ArrPrint(arr);
+        }
+    }
+}
---
+++ CSharp-Study/CSharp-Study/design_pattern/chain_Method.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Study.design_pattern
+{
+    internal class chain_Method
+    {
+        static void Main(string[] args)
+        {
+            GameObject gameObject = new GameObject();
+            gameObject.SetX(1).SetY(2).SetZ(3);
+            gameObject.Print();
+        }
+
+        public class GameObject
+        {
+            private int x;
+            private int y;
+            private int z;
+
+            public GameObject SetX(int x)
+            {
+                this.x = x;
+                return this;
+            }
+
+            public GameObject SetY(int y)
+            {
+                this.y = y;
+                return this;
+            }
+
+            public GameObject SetZ(int z)
+            {
+                this.z = z;
+                return this;
+            }
+
+            public void Print()
+            {
+                Console.WriteLine("x:{0} y:{1} z:{2}", x, y, z);
+            }
+        }
+    }
+}

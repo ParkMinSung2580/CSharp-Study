9ebc4c61c9aac91deec5fffdc554667270080b4f
ParkMinSung2580 <114042812+ParkMinSung2580@users.noreply.github.com>
2025-04-04 16:46:53 +0900

    design_pattern
    디자인 패턴 - 팩토리,빌더 원리 및 개념 이해, 실습

---
+++ CSharp-Study/CSharp-Study/design_pattern/builder.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Study.design_pattern
+{
+    internal class builder
+    {
+        static void Main(string[] args)
+        {
+            MonsterBuilder orcArcherBuilder = new MonsterBuilder();
+            orcArcherBuilder
+                .SetName("오크 아처")
+                .SetWeapon("나무 활")
+                .SetArmor("가죽 갑옷");
+
+            MonsterBuilder orcWarriorBuilder = new MonsterBuilder();
+            orcWarriorBuilder
+                .SetName("오크 전사")
+                .SetWeapon("+99강 막대기")
+                .SetArmor("미스릴 갑옷");
+
+            Monster monster0 = orcArcherBuilder.Build();
+            Monster monster1 = orcWarriorBuilder.Build();
+
+            Console.WriteLine("{0} {1} {2}", monster0.name, monster0.weapon, monster0.armor);
+            Console.WriteLine("{0} {1} {2}", monster1.name, monster1.weapon, monster1.armor);
+        }
+    }
+        //builder pattern
+    public class MonsterBuilder
+    {
+        public string name;
+        public string weapon;
+        public string armor;
+
+        public MonsterBuilder()
+        {
+            name = "몬스터";
+            weapon = "기본무기";
+            armor = "기본갑옷";
+        }
+
+        public Monster Build()
+        {
+            Monster monster = new Monster();
+            monster.name = name;
+            monster.weapon = weapon;
+            monster.armor = armor;
+
+            return monster;
+        }
+
+        public MonsterBuilder SetName(string name)
+        {
+            this.name = name;
+            return this;
+        }
+
+        public MonsterBuilder SetWeapon(string weapon)
+        {
+            this.weapon = weapon;
+            return this;
+        }
+
+        public MonsterBuilder SetArmor(string armor)
+        {
+            this.armor = armor;
+            return this;
+        }
+    }
+
+    public class Monster
+    {
+        public string name;
+        public string weapon;
+        public string armor;
+    }
+
+ }
---
+++ CSharp-Study/CSharp-Study/design_pattern/practice_250404.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CSharp_Study.design_pattern
+{
+    //프로토 타입
+    internal class practice_250404
+    {
+        static void Main()
+        {
+            WeaponFactory weaponFactory = new WeaponFactory();
+            Weapon ironSword = weaponFactory.Create("철검","노말");
+            Weapon woodSpear =  weaponFactory.Create("나무창","레어");
+            Weapon ironAxe = weaponFactory.Create("쇠도끼","전설");
+
+            ironSword.Print();
+            woodSpear.Print();
+            ironAxe.Print();
+            try
+            {
+                // 예외가 발생할 수 있는 코드
+                //Weapon woodStick = weaponFactory.Create("나무막대기", "신화");
+                Weapon ironSword2 = weaponFactory.Create("철검", "신화");
+            }
+            catch (ArgumentException ex) //매개변수 충족 하지않을 때 던져짐
+            {
+                Console.WriteLine("에러 발생: " + ex.Message);
+            }
+        }
+
+        public class WeaponFactory
+        {
+
+            public Weapon Create(string name,string rarity) {
+                Weapon weapon;
+
+                switch (name)
+                {
+                    case "철검": weapon = new Weapon("철검",300,3); break;
+                    case "나무창": weapon = new Weapon("나무창", 200, 5); break;
+                    case "쇠도끼": weapon = new Weapon("쇠도끼",500,1); break;
+                    default:
+                        //throw new ArgumentException("존재하지 않는 무기 이름입니다: {0}",name);
+                        /*throw new ArgumentException(string, string) 생성자는 포맷 문자열이 아니라,
+                        첫 번째는 메시지, 두 번째는 "내부 파라미터" 용으로 쓰기 때문입니다.*/
+                        throw new ArgumentException($"존재하지 않는 무기 이름입니다: {name}");                        
+                }
+                weapon.SetRarity(rarity);
+                return weapon;
+            }
+        }
+
+        public class Weapon
+        {
+            private string name;
+            private int attackPoint;
+            private int attackRange;
+
+            public Weapon(string name,int ackPoint,int ackRange) { 
+                this.name = name; this.attackPoint = ackPoint; this.attackRange = ackRange; }
+
+            //Damage changes based on rarity
+            public void SetRarity(string rarity)
+            {
+                switch (rarity)
+                {
+                    case "노말": 
+                        this.attackPoint += attackPoint * 0; break;
+                    case "레어":
+                        this.attackPoint += (int)(attackPoint * 0.05); break;
+                    case "희귀":
+                        this.attackPoint += (int)(attackPoint * 0.1); break;
+                    case "전설":
+                        this.attackPoint += (int)(attackPoint * 0.2); break;
+                    default:
+                        //throw new ArgumentException("존재하지 않는 등급입니다: {0}", rarity);
+                        throw new ArgumentException($"존재하지 않는 등급입니다: {rarity}");
+                }
+            }
+
+            public void Print()
+            {
+                Console.WriteLine("이름 : {0}, 무기 공격력 : {1}, 무기 공격범위 : {2}", name, attackPoint, attackRange);
+            }
+        }
+    }
+}
